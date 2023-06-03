cdc9307e7879e6be2bee6dd3fbcac6ce3424cdab
lawrence <kofiarkoh0@gmail.com>
2023-06-03 19:32:47 +0000

    store upload files in images dir

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, UploadFile
+import aiofiles
 
 app = FastAPI()
 
+base_files_dir = '/Users/lawrence/Desktop/works/dog_cats_app_backend/images'
+
 
 @app.post('/classify')
-async def classifyImage():
-    return {"message": "cat and dog classifier"}
+async def classifyImage(image:  UploadFile):
+    async with aiofiles.open(f"{base_files_dir}/{image.filename}", 'wb') as out_file:
+        content = await image.read()
+        await out_file.write(content)
+
+    return {"message": "cat and dog classifier", "file": image}
